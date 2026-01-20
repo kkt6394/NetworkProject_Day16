837609f86b9dcfa8dd67102d427cdcee0b64d44d
kkt6394 <kkt6394@gmail.com>
2026-01-20 16:59:13 +0900

    Initial Commit

---
+++ NetworkProject_Day16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkProject_Day16
+//
+//  Created by 김기태 on 1/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
