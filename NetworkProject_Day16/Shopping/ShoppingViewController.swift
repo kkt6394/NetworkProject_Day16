f58c7a1f83e0b52f81b4c2b4e3a56a54ef9192eb
kkt6394 <kkt6394@gmail.com>
2026-01-21 17:59:24 +0900

    feat: 쇼핑 첫번째 화면 UI 구현

---
+++ NetworkProject_Day16/Shopping/ShoppingViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ShoppingViewController.swift
+//  NetworkProject_Day16
+//
+//  Created by 김기태 on 1/21/26.
+//
+
+import UIKit
+import SnapKit
+
+class ShoppingViewController: UIViewController {
+
+    lazy var searchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "브랜드, 상품, 프로필, 태그 등"
+        searchBar.delegate = self
+        return searchBar
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+}
+
+extension ShoppingViewController: ViewDesign {
+    func configureUI() {
+        view.backgroundColor = .black
+        navigationItem.title = "도봉러의 쇼핑쇼핑"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        [
+            searchBar
+        ].forEach { view.addSubview($0) }
+                
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.horizontalEdges.equalToSuperview().offset(10)
+        }
+    }
+    
+    
+}
+
+extension ShoppingViewController: UISearchBarDelegate {
+    
+}
--- NetworkProject_Day16/ViewController.swift
+++ NetworkProject_Day16/ViewController.swift
@@ -8,30 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let vc1 = RandomViewController()
-    let vc2 = BoxOfficeViewController()
-
 
     @IBOutlet var randomBtn: UIButton!
     
     @IBOutlet var boxOfiiceBtn: UIButton!
     
+    @IBOutlet var shoppingBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         randomBtn.backgroundColor = .systemRed
         boxOfiiceBtn.backgroundColor = .systemGreen
+        shoppingBtn.backgroundColor = .systemYellow
     }
 
     @IBAction func randomBtnTapped(_ sender: UIButton) {
-        
+        let vc1 = RandomViewController()
+
         navigationController?.pushViewController(vc1, animated: true)
     }
     
     @IBAction func boxOfficeBtnTapped(_ sender: UIButton) {
-        
+        let vc2 = BoxOfficeViewController()
+
         navigationController?.pushViewController(vc2, animated: true)
 
     }
+    
+    @IBAction func shoppingBtnTapped(_ sender: UIButton) {
+        let vc = ShoppingViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
 }
 
